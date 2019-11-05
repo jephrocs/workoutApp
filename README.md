9fd4c930b2072c85862271eae8cf5343bf9edd93
Jeff <jpconwi@gmail.com>
2019-11-04 19:55:12 -0800

    starting project not sure where to go

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # workoutApp
 Just a workout app I'm planning to use and test while im at the gym 
+
+
+yarn install:
+yarn add react-native@0.55.4 react-native-web@0.10.0 react-art@16.8.2
---
+++ workoutapp/src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { View, Text} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>
+        Hello bitch
+      </Text>
+    </View>
+  );
+}
+
+export default App;
